6ab00a7aec7a0a8a028ef1b0943d484268bcb505
Omar Abi Farraj <omar.7tech@gmail.com>
2026-06-17 23:10:33 +0300

    make categories table

--- database/migrations/2026_06_17_182738_create_categories_table.php
+++ database/migrations/2026_06_17_182738_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->unsignedSmallInteger('sort_order')->default(0);
+            $table->json('addons')->nullable(); // it should have name and price repeater in filament
             $table->timestamps();
         });
     }
