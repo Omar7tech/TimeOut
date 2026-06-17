cff66ead8e399017d45b97e1dd753e18169cfd67
Omar Abi Farraj <omar.7tech@gmail.com>
2026-06-17 19:28:21 +0300

    remove layout

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,11 +1,9 @@
 import { createInertiaApp } from '@inertiajs/react';
-import AppLayout from '@/layouts/app-layout';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    layout: () => AppLayout,
     progress: {
         color: '#4B5563',
     },
