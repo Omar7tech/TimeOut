673bc26295562d087daadfe769530d0c8556d194
Omar Abi Farraj <omar.7tech@gmail.com>
2026-06-19 09:45:52 +0300

    enhance the general settings

--- app/Filament/Pages/ManageGeneral.php
+++ app/Filament/Pages/ManageGeneral.php
@@ -7,6 +7,7 @@ use BackedEnum;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Pages\SettingsPage;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 
@@ -20,10 +21,25 @@ class ManageGeneral extends SettingsPage
     {
         return $schema
             ->components([
-                TextInput::make('lbp_exchange_rate')
-                    ->numeric(),
-                Toggle::make('show_lbp_prices')
-                    ->required(),
+                Section::make('LBP Pricing')
+                    ->description('Configure whether prices are also shown in Lebanese Pounds.')
+                    ->icon(Heroicon::OutlinedCurrencyDollar)
+                    ->schema([
+                        Toggle::make('show_lbp_prices')
+                            ->label('Show LBP prices')
+                            ->helperText('Enable to display prices converted to Lebanese Pounds.')
+                            ->live(),
+                        TextInput::make('lbp_exchange_rate')
+                            ->label('LBP exchange rate')
+                            ->helperText('Amount of LBP per 1 USD.')
+                            ->numeric()
+                            ->minValue(0)
+                            ->suffix('LBP')
+                            ->requiredIf('show_lbp_prices', true)
+                            ->visibleJs(<<<'JS'
+                                $get('show_lbp_prices')
+                                JS),
+                    ]),
             ]);
     }
 }
