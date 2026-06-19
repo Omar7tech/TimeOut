2afd4badd6872ad9b4154d5bf05f782f594ad52f
Omar Abi Farraj <omar.7tech@gmail.com>
2026-06-19 11:00:20 +0300

    sdf

--- resources/js/pages/menu.tsx
+++ resources/js/pages/menu.tsx
@@ -12,11 +12,11 @@ export default function Menu({ orderTypeLabel }: MenuProps) {
         <>
             <Head title={`${orderTypeLabel} Menu`} />
 
-            <header className="flex items-center justify-between px-4 py-3">
+            <header className="flex items-center justify-between px-4 py-3 max-w-7xl mx-auto">
                 <img
                     src="/logos/timeout-logo.png"
                     alt="Time Out Snack"
-                    className="h-10 select-none"
+                    className="h-10 select-none md:h-16"
                     draggable={false}
                 />
 
